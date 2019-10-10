cbd242adeebb7af84e1d33c4eb826e94023926c1
micheal <mojemoron2009@yahoo.com>
2019-10-10 06:36:00 +0100

    modify url routes

--- GithubAPI/urls.py
+++ GithubAPI/urls.py
@@ -13,7 +13,7 @@ urlpatterns += [
     url(r'^events$', views.ListCreateEvent.as_view(), name='event-list'),
     url(r'^events/actors/(?P<actor>.+)/$', views.RetrieveUpdateDestroyEvent.as_view(), name='event-detail'),
     url(r'^actors$', views.ListCreateActor.as_view(), name='actor-list'),
-    url(r'^actors/(?P<pk>[0-9]+)/$', views.RetrieveUpdateDestroyActor.as_view(), name='actor-detail'),
+    url(r'^actors/(?P<pk>[0-9]+)$', views.RetrieveUpdateDestroyActor.as_view(), name='actor-detail'),
     url(r'^repos$', views.ListCreateRepo.as_view(), name='repo-list'),
     url(r'^repos/(?P<id>[0-9]+)/$', views.RetrieveUpdateDestroyRepo.as_view(), name='repo-detail')
 
